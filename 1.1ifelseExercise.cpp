b1eea2e07dd0014392c33be68d2174e2dd51a537
hero <2020104216@qq.com>
2022-04-22 23:18:03 +0800

    2022/4/22/23:17

---
+++ 1.1ifelseExercise.cpp
@@ -0,0 +1,10 @@
+//#include<stdio.h>
+//void main() {
+//	int a = 0;//初始化变量a=0
+//	int b = 0;//初始化变量b=0
+//	scanf_s("%d", &a);//扫描输入,并把值赋给a
+//	scanf_s("%d", &b);//扫描输入，把值赋给b
+//	if (a + b > 50) {//条件判断，如果a+b>50就打印输出hello world
+//		printf("hello world");
+//	}
+//}
---
+++ 1.2ifelseExerice.cpp
@@ -0,0 +1,27 @@
+////声明两个double变量并进行赋值，判断一个数大于10.0，且第二个数小于20.0，打印两数之和
+////第一种解法
+///*#include<stdio.h>
+//void main() {
+//	float a = 0.0;
+//	float b = 0.0;
+//	scanf_s("%f", &a);
+//	scanf_s("%f", &b);
+//	if (a > 10.0) {
+//		if (b < 20.0) {
+//			printf("a+b=%f", a + b);
+//		}
+//	}
+//}
+//*/
+//
+////第二种解法
+//#include<stdio.h>
+//void main() {
+//	float a = 0.0;
+//	float b = 0.0;
+//	scanf_s("%f", &a);
+//	scanf_s("%f", &b);
+//	if (a > 10.0 && b < 20.0) {
+//		printf("a+b=%f", a + b);
+//	}
+//}
