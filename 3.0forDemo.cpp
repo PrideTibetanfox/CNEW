289809a3809df19d338a649f869756de060fdf07
hero <2020104216@qq.com>
2022-04-23 15:14:13 +0800

    3.0forDemo.cpp

---
+++ 3.0forDemo.cpp
@@ -0,0 +1,37 @@
+//#include<stdio.h>
+//void main() {
+//	//int i = 0;
+//	//for (i = 0; i <= 5; i++) {
+//	//	printf("hello world i =%d\n",i);
+//
+//	//}
+//	//写法2
+////	int i = 1;
+////	for (; i <= 5; ) {
+////		printf("hello world i = %d\n", i);
+////		i++;
+////	}
+////}
+///*
+//hello world i =0
+//hello world i =1
+//hello world i =2
+//hello world i =3
+//hello world i =4
+//hello world i =5
+//*/
+//
+////循环初始值可以有多条初始化语句，但要求类型一样，并且中间用逗号隔开
+////循环变量也可以有多条变量迭代语句，中间用逗号隔开
+//	int i;
+//	int j;
+//	for (i = 0, j = 0; i <= 3; i++,j+=2) {
+//		printf("\ni=%d,j=%d", i, j);
+//	}
+//}
+///*
+//i=0,j=0
+//i=1,j=2
+//i=2,j=4
+//i=3,j=6
+//*/
