fb075b4b5b5973357f2574bdc8cc4d0d39cfbeb0
hero <2020104216@qq.com>
2022-04-23 15:13:45 +0800

    2.1switchexercise

---
+++ 2.1switchExercise.cpp
@@ -0,0 +1,16 @@
+//#include<stdio.h>
+//void main() {
+//	double score = 0.0;
+//	scanf_s("%lf", &score);
+//	switch (int(score/60)) {
+//	case 0:
+//		printf("不合格");
+//		break;
+//	case1:
+//		printf("合格");
+//		break;
+//	default:
+//		printf("输入的成绩有误");
+//		break;
+//	}
+//}
