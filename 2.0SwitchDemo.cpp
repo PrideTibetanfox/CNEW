0632c9d3b59ab34722d1c779a2bccc1e048011a3
hero <2020104216@qq.com>
2022-04-23 13:22:05 +0800

    2.0switchdemo

---
+++ 2.0SwitchDemo.cpp
@@ -0,0 +1,35 @@
+///*
+//编写一个程序，该程序可以接收一个字符，如a,,b,c,d,e,f,g
+//a表示星期1，
+//b表示星期2....
+//根据用户的输入显示对应的信息
+//*/
+//
+////==思路==
+////用switch语句 
+//#include<stdio.h>
+//void main() {
+//	printf("请输入一个字符(a,b,c,d)");
+//	char c1 = ' ';
+//	scanf_s("%c", &c1);
+//	switch (c1) {
+//	case 'a':
+//		printf("今天星期一");
+//		break;
+//	case 'b':
+//		printf("今天星期二");
+//		//break;
+//	case 'c':
+//		printf("今天星期三");
+//		break;
+//	case 'd':
+//		printf("今天星期四");
+//		break;
+//	case 'e':
+//		printf("今天星期五");
+//		break;
+//	default:
+//		printf("今天星期天");
+//		break;
+//	}
+//}
