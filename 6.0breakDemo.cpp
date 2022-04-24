6b978be5a1f7fbb7ec83e40adf681a328a6008ba
hero <2020104216@qq.com>
2022-04-24 23:39:21 +0800

    6.0breakDemo.cpp

---
+++ 6.0breakDemo.cpp
@@ -0,0 +1,13 @@
+//#include<stdio.h>
+//void main() {
+//	int i = 0;
+//	for (i = 0; i < 10; i++) {
+//		if (i == 3) { break; }//break语句用于终止某个语句块的执行,i==3时，终止，不再执行
+//		printf("i=%d\n",i);
+//	}
+//}
+///*
+//i=0
+//i=1
+//i=2
+//*/
