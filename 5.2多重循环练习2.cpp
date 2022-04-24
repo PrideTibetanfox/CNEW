06f9ce8293fafe0b3931a37f10fcefe7e3b0babb
hero <2020104216@qq.com>
2022-04-24 19:45:10 +0800

    5.2多重循环练习2.cpp

---
+++ 5.2多重循环练习2.cpp
@@ -0,0 +1,12 @@
+////打印99乘法表
+////分析，用循环语句
+//#include<stdio.h>
+//void main() {
+//	int i, j;
+//	for (i = 1; i <= 9; i++) {
+//		for (j = 1; j <= i; j++) {
+//			printf("%d*%d=%d  ",j, i, i*j);
+//		}
+//		printf("\n");
+//	}
+//}
