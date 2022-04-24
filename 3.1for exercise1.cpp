cb2212785e324e49fb9f6edf223037dfb9c46dc1
hero <2020104216@qq.com>
2022-04-24 15:02:58 +0800

    3.1for exercise1.cpp

---
+++ 3.1for exercise1.cpp
@@ -0,0 +1,29 @@
+////1-100之间所有是9的倍数的个数及其总和
+//#include<stdio.h>
+//void main() {
+//	int i = 0;
+//	int count = 0;
+//	int sum = 0;
+//	for (i = 0; i <= 100; i++) {
+//		if (i % 9 == 0) {
+//			count++;
+//			sum += i;
+//			printf("i=%d\n",i);
+//		}
+//	}printf("sum=%d,count=%d",  sum,count);
+//}
+//
+///*
+//i=0
+//i=9
+//i=18
+//i=27
+//i=36
+//i=45
+//i=54
+//i=63
+//i=72
+//i=81
+//i=90
+//i=99
+//sum=594,count=12*/
