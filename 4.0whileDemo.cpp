4fb3fbb070802a80877d50dd4f1144deec645b19
hero <2020104216@qq.com>
2022-04-24 16:18:12 +0800

    4.0whileDemo.cpp

---
+++ 4.0whileDemo.cpp
@@ -0,0 +1,10 @@
+////用while循环语句来实现打印五次"hello world "
+//#include<stdio.h>
+//void main() {
+//	int i = 0;//1.循环变量初始化
+//	while (i<=5) {//2.循环条件
+//		printf("hello world\n");//3.循环语句
+//		i++;//4.循环变量的迭代
+//
+//	}
+//}
