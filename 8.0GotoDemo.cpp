1a7182e291a86520b97c551f4bad78f4149ad220
hero <2020104216@qq.com>
2022-04-27 19:10:19 +0800

    8.0GotoDemo.cpp

---
+++ 8.0GotoDemo.cpp
@@ -0,0 +1,18 @@
+//#include<stdio.h>
+//void main() {
+//	printf("hello 1\n");
+//	goto lable1;
+//	printf("ok1\n");
+//	printf("ok2\n");
+//lable1:
+//	printf("ok3\n");
+//	printf("ok4\n");
+//lable2:
+//	printf("hello 2");
+//}
+///*
+//hello 1
+//ok3
+//ok4
+//hello 2
+//*/
